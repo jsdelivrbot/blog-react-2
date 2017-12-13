9f01c9b925e5b374a21190e54f83db10ade72103
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-13 15:50:47 +1000

    added post submitting and error handling

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,33 +11,54 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     )
   }
+  onSubmit(values) {
+    console.log(values);
+  }
 
   render(){
+    const {handleSubmit} = this.props
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
-          label="Title"
+          label="Title for Post"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
           label="Post Content"
-          name="Content"
+          name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {}
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters!"
+  }
+  if (!values.categories) {
+    errors.categories = "Enter a category"
+  }
+  if (!values.content) {
+    errors.content = "Enter Content"
+  }
+  return errors
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 }) (PostsNew)
