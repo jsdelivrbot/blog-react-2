930f33ddfea49fd5fb76d627a5d14c2cde5845ef
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-13 20:30:18 +1000

    wired post request to api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,23 @@
 import axios from 'axios'
 
-export const FETCH_POSTS = fetchPosts
+export const FETCH_POSTS = 'fetch_posts'
+export const CREATE_POST = 'create_post'
+
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=Goodtimes1234'
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
