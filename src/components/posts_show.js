98472d4a41328e07384efaa5e4cb1779952af0a6
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-14 20:29:01 +1000

    added posts show

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchPost} from '../actions'
+
 
 class PostsShow extends Component {
+
   render() {
+    posts[this.props.match.params.id]
     return (
       <div>
-        Posts Show
+
       </div>
     )
   }
+  componentDidMount() {
+    const {id} = this.props.match.params.id
+    this.props.fetchPost(id)
+  }
 }
+
+function mapStateToProps({posts}) {
+  return {posts}
+}
+export default connect(null, {fetchPost}) (PostsShow)
